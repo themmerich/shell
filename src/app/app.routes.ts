cb595af336ef2fc55f77b1f4ea0e3fb6cac15d81
hemm <Thomas.Hemmerich.Extern@parcIT.de>
2025-07-21 11:17:36 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {loadRemoteModule} from '@angular-architects/native-federation';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'mfe1',
+    loadComponent: () => loadRemoteModule('mfe1', './Component')
+  },
+  {
+    path: 'mfe2',
+    loadComponent: () => loadRemoteModule('mfe2', './Component')
+  }
+];
